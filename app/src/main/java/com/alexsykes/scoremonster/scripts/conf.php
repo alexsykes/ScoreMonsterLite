c7a305e11291c16d9bd160d34cc82cf00624847d
Alex Sykes <alex@alexsykes.net>
2020-06-09 09:29:59 +0100

    Prepare for release in progress

---
+++ app/src/main/java/com/alexsykes/scoremonster/scripts/conf.php
@@ -0,0 +1,15 @@
+<?php
+//these are the server details
+//the username is root by default in case of xampp
+//password is nothing by default
+//and lastly we have the database named android. if your database name is different you have to change it 
+$servername = // "localhost";
+$username = "DATABASE_USER";
+$password = "DATABASE_PASSWORD";
+$database = "DATABASE_NAME";
+$dbprefix = "GOBBLE_DE_GOOK"; // JOOMLA table prefix
+
+$smtpAddress = 'SMTP_ADDRESS';
+$smtpPassword = 'SMTP_PASSWORD';
+$smtpHost = 'SMTP_HOST';
+?>
