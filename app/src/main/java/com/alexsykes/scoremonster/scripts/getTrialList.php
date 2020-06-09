70ff85497334a30506b586cd1a18451c06abaecd
Alex Sykes <alex@alexsykes.net>
2020-06-09 09:08:32 +0100

    Prepare for release in progress

--- app/src/main/java/com/alexsykes/scoremonster/scripts/getTrialList.php
+++ app/src/main/java/com/alexsykes/scoremonster/scripts/getTrialList.php
@@ -21,20 +21,20 @@ $trials = array();
 
 // $sql = "SELECT id, eventname, date, club, numlaps, numsections  FROM up93k_entryman_trial AS t WHERE t.date BETWEEN (NOW() - INTERVAL 90 DAY)  AND (NOW() + INTERVAL 7 DAY)  AND t.published = 1 ORDER BY `date` DESC";
 
-$sql = "SELECT id, eventname, date, club, numlaps, numsections, starttime  FROM up93k_entryman_trial AS t WHERE t.isscoringlocked = 0 AND t.published = 1 ORDER BY `date` DESC";
+$sql = "SELECT id, eventname, date, club, numlaps, numsections, starttime, email  FROM ".$dbprefix."entryman_trial AS t WHERE t.isscoringlocked = 0 AND t.published = 1 ORDER BY `date` DESC";
 //creating an statment with the query
 $stmt = $conn->prepare($sql);
 
 //executing that statment
 $stmt->execute();
 
-//binding results for that statment 
-$stmt->bind_result($id, $eventname, $date, $club, $numlaps, $numsections, $starttime);
+//binding results for that statment
+$stmt->bind_result($id, $eventname, $date, $club, $numlaps, $numsections, $starttime, $email);
 
 //looping through all the records
 while($stmt->fetch()){
-	
-	//pushing fetched data in an array 
+
+	//pushing fetched data in an array
 	$temp = [
 		'id'=>$id,
 		'date'=>$date,
@@ -42,7 +42,8 @@ while($stmt->fetch()){
 		'name'=>$eventname,
 		'numlaps'=>$numlaps,
 		'numsections'=>$numsections,
-		'starttime'=>$starttime
+		'starttime'=>$starttime,
+		'email' => $email
 	];
 	
 	//pushing the array inside the hero array 
