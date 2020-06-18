222eb3f0648f5bb4701b2ef4061a5e292bf4a907
Alex Sykes <alex@alexsykes.net>
2020-06-18 09:50:19 +0100

    Prior to adding webView for Help system

--- app/src/main/java/com/alexsykes/scoremonster/activities/HelpActivity.java
+++ app/src/main/java/com/alexsykes/scoremonster/activities/HelpActivity.java
@@ -6,8 +6,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.text.Html;
 
@@ -29,6 +34,19 @@ public class HelpActivity extends AppCompatActivity {
         // helpButton.setEnabled(false);
         //  helpButton.setBackgroundColor(getResources().getColor(R.color.colorWhite));
         helpTextView.setText(Html.fromHtml(getString(R.string.helpApp)));
+
+/*        LinearLayout scrollView = findViewById(R.id.layout);
+        // Create TextView programmatically.
+        TextView textView = new TextView(this);
+        textView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        textView.setGravity(Gravity.CENTER);
+        textView.setText("Added");
+        scrollView.addView(textView);
+        ImageView mainScreenView = new ImageView(this);
+        mainScreenView.setMaxHeight(12);
+        mainScreenView.setImageDrawable(getResources().getDrawable(R.drawable.main_screen, null));
+        scrollView.addView(mainScreenView);*/
+
     }
 
     public void showHelp(View view) {
