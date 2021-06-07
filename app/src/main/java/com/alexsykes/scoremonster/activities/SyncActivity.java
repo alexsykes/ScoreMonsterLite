3cd54aa2f1f2ee63e05cf0550bf42082f23e3a5a
Alex Sykes <alex@alexsykes.net>
2021-06-07 20:27:03 +0100

    minor

--- app/src/main/java/com/alexsykes/scoremonster/activities/SyncActivity.java
+++ app/src/main/java/com/alexsykes/scoremonster/activities/SyncActivity.java
@@ -57,7 +57,6 @@ public class SyncActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync);
 
