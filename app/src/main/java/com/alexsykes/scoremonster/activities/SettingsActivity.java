fd5eb892985e77044afe4958b49a425d475ed0d3
Alex Sykes <alex@alexsykes.net>
2021-08-09 08:13:25 +0100

    Settings Activity added

--- app/src/main/java/com/alexsykes/scoremonster/activities/SettingsActivity.java
+++ app/src/main/java/com/alexsykes/scoremonster/activities/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.alexsykes.scoremonster.activities;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.alexsykes.scoremonster.R;
@@ -30,6 +34,70 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            EditTextPreference mobilePref = findPreference("mobile");
+            assert mobilePref != null;
+            mobilePref.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_PHONE);
+                }
+            });
+
+            EditTextPreference sectionPref = findPreference("section");
+            assert sectionPref != null;
+            sectionPref.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                }
+            });
+
+            EditTextPreference numSectionsPref = findPreference("numsections");
+            assert numSectionsPref != null;
+            numSectionsPref.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                }
+            });
+
+            EditTextPreference numLapsPref = findPreference("numlaps");
+            assert numLapsPref != null;
+            numLapsPref.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                }
+            });
+
+            EditTextPreference trialidPref = findPreference("trialid");
+            assert trialidPref != null;
+            trialidPref.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                }
+            });
+
+            EditTextPreference emailPref = findPreference("email");
+            assert emailPref != null;
+            emailPref.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_CLASS_TEXT |
+                            InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                }
+            });
+
+            EditTextPreference observerPref = findPreference("observer");
+            assert observerPref != null;
+            observerPref.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+                @Override
+                public void onBindEditText(@NonNull EditText editText) {
+                    editText.setInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+                }
+            });
         }
     }
 }
