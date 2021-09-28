04c36376c3cf621d79df7e840f28088b2dc596d5
Alex Sykes <alex@alexsykes.net>
2021-09-28 11:54:12 +0100

    Timer Activity WiP

--- app/src/main/java/com/alexsykes/scoremonster/activities/TimerResetActivity.java
+++ app/src/main/java/com/alexsykes/scoremonster/activities/TimerResetActivity.java
@@ -1,14 +1,21 @@
 package com.alexsykes.scoremonster.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 
 import com.alexsykes.scoremonster.R;
 
 public class TimerResetActivity extends AppCompatActivity {
+    CheckBox confirmResetTimer, confirmDeleteScores;
+    SwitchCompat deleteTimes, resetTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,39 @@ public class TimerResetActivity extends AppCompatActivity {
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+
+        confirmResetTimer = findViewById(R.id.confirmTimeReset);
+        confirmDeleteScores = findViewById(R.id.confirmDeleteTimes);
+        deleteTimes = findViewById(R.id.deleteTimes);
+        resetTimer = findViewById(R.id.resetTimer);
+        deleteTimes.setChecked(false);
+        resetTimer.setChecked(false);
+
+        confirmResetTimer.setVisibility(View.GONE);
+        confirmDeleteScores.setVisibility(View.GONE);
+
+        resetTimer.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    confirmResetTimer.setVisibility(View.VISIBLE);
+                } else {
+                    confirmResetTimer.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        deleteTimes.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    confirmDeleteScores.setVisibility(View.VISIBLE);
+                } else {
+                    confirmDeleteScores.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
     }
 }
