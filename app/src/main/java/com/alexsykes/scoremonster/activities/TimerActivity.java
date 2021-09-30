49a2a71da6fc6e2caa6728be869d2ac6f07cce0d
Alex Sykes <alex@alexsykes.net>
2021-09-30 08:00:15 +0100

    Elapsed time calculation - WiP

--- app/src/main/java/com/alexsykes/scoremonster/activities/TimerActivity.java
+++ app/src/main/java/com/alexsykes/scoremonster/activities/TimerActivity.java
@@ -39,7 +39,7 @@ import java.util.Calendar;
 public class TimerActivity extends AppCompatActivity {
     NumberPadFragment numberPadFragment;
     LinearLayout content;
-    TextView numberLabel, statusLine;
+    TextView riderNumberLabel, statusLine;
     SharedPreferences localPrefs;
     Button finishButton, startClockButton;
     long clockStartTime, startInterval;
@@ -52,127 +52,39 @@ public class TimerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i("Info", "onCreate called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        // Add custom ActionBar
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.getMenu();
 
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
 
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
+        numberPadFragment = new NumberPadFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.content, numberPadFragment).commit();
+
+        // Add widgets
+        UISetup();
 
         // Load up preferences for trialid, riderNumber, clockStartTime
         getPrefs();
-        UISetup();
-        getSupportFragmentManager().beginTransaction().add(R.id.content, numberPadFragment).commit();
-        if (clockStartTime == 0) {
-            getSupportFragmentManager().beginTransaction().remove(numberPadFragment).commit();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("Info", "onResume called");
-    }
-
-    @Override
-    protected void onPause() {
-        Log.i("Info", "onPause called");
-        super.onPause();
-        saveCurrentState();
-    }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.timer_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Show scores on remote server
-            case R.id.timesheet:
-                goTimesheet();
-                return true;
-
-            // Enter andinitialise section details
-            case R.id.setup:
-                goSetup();
-                return true;
-
-            default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void goTimesheet() {
-        Intent intent = new Intent(this, TimeListActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    private void goSetup() {
-        Intent intent = new Intent(this, TimerResetActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    private void saveCurrentState() {
-        Log.i("Info", "saveCurrentState called");
-        localPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = localPrefs.edit();
-        if (clockStartTime > 0) {
-            String currentRiderText = numberLabel.getText().toString();
-            int rider = 0;
-            if (!currentRiderText.equals("")) {
-                rider = Integer.parseInt(numberLabel.getText().toString());
-            }
-            editor.putInt("ridingNumber", rider);
-            editor.apply();
-        }
     }
 
     private void UISetup() {
-        // Add custom ActionBar
-        Toolbar myToolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-        myToolbar.getMenu();
-
         statusLine = findViewById(R.id.statusLine);
         startClockButton = findViewById(R.id.startClockButton);
-        startClockButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                startClock(this);
-                return false;
-            }
-
-            private void startClock(View.OnLongClickListener onLongClickListener) {
-                Calendar startTime = Calendar.getInstance();
-                clockStartTime = startTime.getTimeInMillis();
-
-                // Save start time in prefs
-                SharedPreferences.Editor editor = localPrefs.edit();
-                editor.putLong("clockStartTime", clockStartTime);
-                editor.apply();
-                statusLine.setText("Clock started");
-                getSupportFragmentManager().beginTransaction().replace(R.id.content, numberPadFragment).commit();
-                startClockButton.setVisibility(View.GONE);
-                numberLabel.setText("");
-                SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm:ss a");
-                String dateString = dateFormat.format(clockStartTime);
-                statusLine.setText("Start time: " + dateString);
-            }
-        });
-        numberPadFragment = new NumberPadFragment();
         content = findViewById(R.id.content);
-        numberLabel = findViewById(R.id.riderNumberLabel);
+        riderNumberLabel = findViewById(R.id.riderNumberLabel);
         finishButton = findViewById(R.id.finishButton);
+
+        // Set listeners
         finishButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -181,23 +93,32 @@ public class TimerActivity extends AppCompatActivity {
             }
         });
 
-        // Button setup
-        if (ridingNumber > 0) {
-            numberLabel.setText(String.valueOf(ridingNumber));
-        } else {
-            numberLabel.setText("");
-        }
-        if (clockStartTime > 0) {
-            startClockButton.setVisibility(View.GONE);
-            finishButton.setVisibility(View.VISIBLE);
-            SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm:ss a");
-            String dateString = dateFormat.format(clockStartTime);
-            statusLine.setText("Start time: " + dateString);
-        } else {
-            startClockButton.setVisibility(View.VISIBLE);
-            finishButton.setVisibility(View.GONE);
-            numberLabel.setText("Clock not started");
-        }
+        startClockButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                startClock(this);
+                return false;
+            }
+
+            // called when button pressed
+            private void startClock(View.OnLongClickListener onLongClickListener) {
+                Calendar startTime = Calendar.getInstance();
+                clockStartTime = startTime.getTimeInMillis();
+
+                // Save start time in prefs
+                SharedPreferences.Editor editor = localPrefs.edit();
+                editor.putLong("clockStartTime", clockStartTime);
+                editor.apply();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm:ss a");
+                String dateString = dateFormat.format(clockStartTime);
+                statusLine.setText("Clock started at " + dateString);
+                content.setVisibility(View.VISIBLE);
+                startClockButton.setVisibility(View.GONE);
+                statusLine.setVisibility(View.VISIBLE);
+                finishButton.setVisibility(View.VISIBLE);
+                riderNumberLabel.setText("");
+            }
+        });
     }
 
     private void getPrefs() {
@@ -205,12 +126,35 @@ public class TimerActivity extends AppCompatActivity {
         trialid = localPrefs.getInt("trialid", 0);
         ridingNumber = localPrefs.getInt("ridingNumber", 0);
         clockStartTime = localPrefs.getLong("clockStartTime", 0);
-        // mode = localPrefs.getInt("mode", 0);
+
+        if (clockStartTime == 0) {
+            statusLine.setVisibility(View.INVISIBLE);
+            startClockButton.setVisibility(View.VISIBLE);
+            finishButton.setVisibility(View.GONE);
+            // riderNumberLabel.setVisibility(View.VISIBLE);
+            riderNumberLabel.setText("Timer not started");
+            content.setVisibility(View.GONE);
+        } else {
+            statusLine.setVisibility(View.VISIBLE);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("h:mm:ss a");
+            String dateString = dateFormat.format(clockStartTime);
+            statusLine.setText("Start time: " + dateString);
+            startClockButton.setVisibility(View.GONE);
+            finishButton.setVisibility(View.VISIBLE);
+            content.setVisibility(View.VISIBLE);
+        }
+
+        // Button setup
+        if (ridingNumber > 0) {
+            riderNumberLabel.setText(String.valueOf(ridingNumber));
+        } else {
+            riderNumberLabel.setText("");
+        }
     }
 
     public void addDigit(View view) {
         // Get length of rider riderNumber
-        String riderNumber = numberLabel.getText().toString();
+        String riderNumber = riderNumberLabel.getText().toString();
         int len = riderNumber.length();
 
         // Get id from clicked button to get clicked digit
@@ -234,7 +178,7 @@ public class TimerActivity extends AppCompatActivity {
         if (riderNumber.equals("0")) {
             riderNumber = "";
         }
-        numberLabel.setText(riderNumber);
+        riderNumberLabel.setText(riderNumber);
     }
 
     private void save(View.OnLongClickListener view) {
@@ -242,7 +186,7 @@ public class TimerActivity extends AppCompatActivity {
 
         ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.MAX_VOLUME);
         // Get String values for rider and scoreLabel
-        String rider = numberLabel.getText().toString();
+        String rider = riderNumberLabel.getText().toString();
         // Get value for time
         Calendar finishTime = Calendar.getInstance();
         long finishTimeInMillis = finishTime.getTimeInMillis();
@@ -308,7 +252,7 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     private void clearScore() {
-        numberLabel.setText("");
+        riderNumberLabel.setText("");
         SharedPreferences.Editor editor = localPrefs.edit();
         editor.putInt("ridingNumber", 0);
         editor.apply();
@@ -319,5 +263,78 @@ public class TimerActivity extends AppCompatActivity {
     public void playSoundFile(Integer fileName) {
         mediaPlayer = MediaPlayer.create(this, fileName);
         mediaPlayer.start();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.timer_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Show scores on remote server
+            case R.id.timesheet:
+                goTimesheet();
+                return true;
+
+            // Enter andinitialise section details
+            case R.id.setup:
+                goSetup();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void goTimesheet() {
+        Intent intent = new Intent(this, TimeListActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    private void goSetup() {
+        Intent intent = new Intent(this, TimerResetActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getPrefs();
+        if (clockStartTime == 0) {
+            statusLine.setVisibility(View.INVISIBLE);
+            startClockButton.setVisibility(View.VISIBLE);
+            finishButton.setVisibility(View.GONE);
+            riderNumberLabel.setText("Timer not started");
+            content.setVisibility(View.GONE);
+        }
+        Log.i("Info", "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i("Info", "onPause called");
+        super.onPause();
+        saveCurrentState();
+    }
+
+    private void saveCurrentState() {
+        Log.i("Info", "saveCurrentState called");
+        localPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = localPrefs.edit();
+        if (clockStartTime > 0) {
+            String currentRiderText = riderNumberLabel.getText().toString();
+            int rider = 0;
+            if (!currentRiderText.equals("")) {
+                rider = Integer.parseInt(riderNumberLabel.getText().toString());
+            }
+            editor.putInt("ridingNumber", rider);
+            editor.apply();
+        }
     }
 }
