c16fb48f305779ac78d32cd07aa7394882e995b9
Alex Sykes <alex@alexsykes.net>
2024-02-01 22:54:06 +0000

    End 01_02_24

--- app/src/main/java/uk/trialmonster/observer/activities/HelpActivity.java
+++ app/src/main/java/uk/trialmonster/observer/activities/HelpActivity.java
@@ -15,7 +15,6 @@ import uk.trialmonster.observer.R;
 public class HelpActivity extends AppCompatActivity {
     TextView helpTextView;
     WebView webView;
-    String helpText;
     Button nsButton, helpButton, spButton;
 
     @Override
@@ -40,54 +39,24 @@ public class HelpActivity extends AppCompatActivity {
         webView.setVisibility(View.VISIBLE);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://trialmonster.uk/index.php?option=com_content&view=article&id=9");
-
-        // helpButton.setEnabled(false);
-        //  helpButton.setBackgroundColor(getResources().getColor(R.color.colorWhite));
-
-/*        LinearLayout scrollView = findViewById(R.id.layout);
-        // Create TextView programmatically.
-        TextView textView = new TextView(this);
-        textView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        textView.setGravity(Gravity.CENTER);
-        textView.setText("Added");
-        scrollView.addView(textView);
-        ImageView mainScreenView = new ImageView(this);
-        mainScreenView.setMaxHeight(12);
-        mainScreenView.setImageDrawable(getResources().getDrawable(R.drawable.main_screen, null));
-        scrollView.addView(mainScreenView);*/
-
     }
 
     public void showPrivacy(View view) {
-//        webView.setVisibility(View.VISIBLE);
-//        helpTextView.setVisibility(View.GONE);
         webView.loadUrl("https://trialmonster.uk/index.php?option=com_content&view=article&id=9");
         webView.getSettings().setJavaScriptEnabled(true);
     }
 
     public void showHelp(View view) {
-//        helpTextView = findViewById(R.id.helpApp);
-//        helpTextView.setText(Html.fromHtml(getString(R.string.helpApp)));
-//        webView.setVisibility(View.GONE);
-//        helpTextView.setVisibility(View.VISIBLE);
         webView.loadUrl("https://trialmonster.uk/index.php?option=com_content&view=article&id=12");
         webView.getSettings().setJavaScriptEnabled(true);
     }
 
     public void showNS(View view) {
-//        helpTextView = findViewById(R.id.helpApp);
-//        helpTextView.setText(Html.fromHtml(getString(R.string.tsr22NS)));
-//        webView.setVisibility(View.GONE);
-//        helpTextView.setVisibility(View.VISIBLE);
         webView.loadUrl("https://trialmonster.uk/index.php?option=com_content&view=article&id=11");
         webView.getSettings().setJavaScriptEnabled(true);
     }
 
     public void showSP(View view) {
-//        helpTextView = findViewById(R.id.helpApp);
-//        helpTextView.setText(Html.fromHtml(getString(R.string.tsr22SP)));
-//        webView.setVisibility(View.GONE);
-//        helpTextView.setVisibility(View.VISIBLE);
         webView.loadUrl("https://trialmonster.uk/index.php?option=com_content&view=article&id=10");
         webView.getSettings().setJavaScriptEnabled(true);
     }
